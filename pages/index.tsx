145758edcbfdd0aaea703b940600e642f500781a
olya-mosunova <o.mosunova@skillline.ru>
2021-07-05 22:14:18 +0200

    add rating component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-import {Htag, Button, P, Tag} from "../components";
+import React, {useState} from "react";
+import {Htag, Button, P, Tag, Rating} from "../components";
 
 export default function Home(): JSX.Element {
+    const [rating, setRating] = useState<number>(4);
+
   return (
     <>
       <Htag tag={'h1'}>Текст</Htag>
-        <Button appearance="primary" arrow="right">Кнопка</Button>
+        <Button
+            appearance="primary"
+            arrow="right"
+        >Кнопка</Button>
         <Button appearance="ghost" arrow="down">Кнопка</Button>
         <P size="s">Всем привет</P>
         <P>Всем привет</P>
@@ -15,6 +20,7 @@ export default function Home(): JSX.Element {
       <Tag size="s" color="green">Green</Tag>
       <Tag color="primary">Primary</Tag>
       <Tag color="grey">Grey</Tag>
+    <Rating rating={rating} isEditable={true} setRating={setRating} />
     </>
   );
 }
