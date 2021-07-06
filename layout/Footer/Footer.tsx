f049f44be78d0e18fe64dd0d1aea05cc21707137
olya-mosunova <o.mosunova@skillline.ru>
2021-07-06 17:05:05 +0200

    footer grid styles

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import {FooterProps} from "./Footer.props";
 import styles from "./Footer.module.css";
 import cn from "classnames";
+import {format} from "date-fns";
 
 export const Footer = (
-    {...props}: FooterProps): JSX.Element => {
+    {
+        className,
+        ...props}: FooterProps): JSX.Element => {
     return (
-        <div {...props}>
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+
+            <a href="#" target="_blank">Пользовательское соглашение</a>
+            <a href="#" target="_blank">Политика конфиденциальности</a>
+        </footer>
     );
 };
